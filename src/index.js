1da1cb7faeff7dc5539cd1fdd20f51ac59bfe239
bel-sndct <79698612+bel-sndct@users.noreply.github.com>
2021-08-29 14:17:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let new_expr = expr.replace(/\s+/g, '');
+    if (new_expr.indexOf('/0', 0) !== -1) {
+        throw new Error('TypeError: Division by zero.');
+    }
+
+    let brackets = 0;
+    for (let i = 0; i < new_expr.length; i++) {
+        if (new_expr[i] === '(') {
+            brackets++;
+        }
+        else if (new_expr[i] === ')') {
+            brackets--;
+        }
+    }
+    if (brackets !== 0) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+
+    let func = new Function(`return ${new_expr}`);
+    return func();
 }
 
 module.exports = {
